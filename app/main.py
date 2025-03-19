87cd3b761b7c8e5ef77f45071e19c2f6a4f812ba
mahdi <mahdiabbasi602@gmail.com>
2025-03-19 15:54:17 +0330

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -10,8 +10,14 @@ def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     clientSocket, clientAddress = server_socket.accept()  # wait for client
     # Send a simple HTTP response
-    response = b"""HTTP/1.1 200 OK\r\n\r\n"""
-    clientSocket.sendall(response)
+    request = clientSocket.recv(1024)  # receive the client request
+    request = request.decode("utf-8")
+    print(f"Received request: {request}")
+    requestLine, headers, body = request.split("\r\n")
+    if requestLine.split()[1] == "/":
+        clientSocket.sendall(b"HTTP/1.1 200 OK\r\n\r\nHello, World!")
+    else:
+        clientSocket.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
 
 
 if __name__ == "__main__":
